134984082caf394e42181ef6a3eef5a19ed2de3a
GabrielMüllerDS <61920654+GabrielMullerDS@users.noreply.github.com>
2022-03-14 21:24:04 -0300

    Create app.js

---
+++ Aula3/Proj03/src/app.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Welcome from './Components/Welcome';
+
+function App() {
+  return (
+    <div>
+      <Welcome nome='Gabriel'/>
+      <Welcome nome='Água'/>
+      <Welcome nome='Sal'/>
+      <Welcome nome='Bolacha'/>
+    </div>
+);}
+
+export default App;
